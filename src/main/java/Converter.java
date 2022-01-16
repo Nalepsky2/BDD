c76851b08391e5ae4136829fad69cbcb5f571c54
Nalepsky2 <marek.b.nalepka@gmail.com>
2022-01-16 22:33:52 +0100

    BDD task

---
+++ src/main/java/Converter.java
@@ -0,0 +1,24 @@
+public class Converter {
+    /*
+    zaimplementuj z wykorzystaniem BDD algorytm który na wejsciu przyjmie dowolny ciąg znaków A-Z
+    i zwróci String w formacie n<znak> oddzielone średnikami,
+    gdzie n oznacza liczbe wystapien danego znaku, w kolejnosci alfabetycznej.
+    pomin n jeśli jest tylko jedno wystapienie
+
+    dla przykładu:
+    abba -> 2A;2B
+    tatar -> 2A;2T;R
+    (2pkt)
+
+    Rozmiar liter nie ma znaczenia
+    (1pkt)
+
+    Jezeli string jest pusty rzuć IllegalArgumentException z wiadomoscią
+    "Input cannot be empty"
+    (1pkt)
+
+    Jeżeli string zawiera znaki specjalne rzuć IllegalArgumentException z wiadomością
+    "Input can contain only letters: found <lista niedozwolonych znaków>"
+    (1pkt)
+     */
+}
